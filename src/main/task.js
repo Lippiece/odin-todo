e9ef3cd24742f57669f2ab1e06af73f30173c0ac
Lippiece <seriouss7222@gmail.com>
2022-08-07 14:55:38 +0200

    feat: reformat task.js and extend addToDom()

--- src/main/task.js
+++ src/main/task.js
@@ -1,23 +1,48 @@
 import { css } from "@emotion/css";
+
+const taskStyle = css( {
+		display        : "flex",
+		flexDirection  : "row",
+		padding        : "1rem",
+		borderRadius   : "0.5rem",
+		width          : "calc(100% - 3em)",
+		height         : "calc(fit-content + 3em)",
+		// boxShadow      : "0 0 0.5rem rgba(255,100,50,1)",
+		backgroundColor: "hsla(15 100% 5% / 50%)",
+	} ),
+	titleStyle = css( {
+		display      : "inline-block",
+		verticalAlign: "top",
+		lineHeight   : "normal",
+		fontSize     : "1.5em",
+		fontWeight   : "bold",
+	} );
 export class Task
 {
 	addToDOM( container )
 	{
 		const task = document.createElement( "div" ),
-			taskStyle = css( {
-				display        : "flex",
-				flexDirection  : "column",
-				justifyContent : "center",
-				alignItems     : "center",
-				margin         : "1rem",
-				padding        : "1rem",
-				borderRadius   : "0.5rem",
-				// boxShadow      : "0 0 0.5rem rgba(255,100,50,1)",
-				backgroundColor: "hsla(15 100% 5% / 50%)",
-			} );
+			icon = document.createElement( "div" ),
+			title = document.createElement( "h3" ),
+			backgroundStyle = {
+				"medium": () => ( task.classList.add( css ( { backgroundColor: "hsla(15 100% 15% / 55%)" } ) ) ),
+				"high"  : () => ( task.classList.add( css ( {
+					backgroundColor: "hsla(15 100% 25% / 60%)",
+					color          : "hsla(15 100% 87% / 80%)",
+				}
+				 ) ) ),
+				"default": () => {},
+			};
 
+		( backgroundStyle[ this.priority ] || backgroundStyle.default )();
+		icon.dataset.icon = "flat-color-icons:medium-priority";
+		icon.classList.add( "iconify" );
+		icon.dataset.width = "2em";
+		title.classList.add( titleStyle );
+		title.textContent = this.title;
+		// title.prepend( icon );
 		task.classList.add( taskStyle );
-		task.append( this.title, this.description, this.priority );
+		task.append( icon, title );
 		container.append( task );
 	}
 	constructor( { title, description, priority, container } )
