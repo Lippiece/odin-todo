b1a925a8aece1aadfd7ec98c19e27e91ed774193
Lippiece <seriouss7222@gmail.com>
2022-08-07 16:44:57 +0200

    feat: add checkbox

--- src/main/task.js
+++ src/main/task.js
@@ -11,18 +11,41 @@ const taskStyle = css( {
 		backgroundColor: "hsla(15 100% 5% / 50%)",
 	} ),
 	titleStyle = css( {
-		display      : "inline-block",
-		verticalAlign: "top",
-		lineHeight   : "normal",
-		fontSize     : "1.5em",
-		fontWeight   : "bold",
+		display   : "inline-block",
+		alignSelf : "center",
+		lineHeight: "150%",
+		fontSize  : "1.5em",
+		fontWeight: "bold",
+	} ),
+	checkStyle = css( {
+		appearance         : "none",
+		display            : "grid",
+		placeItems         : "center",
+		verticalAlign      : "top",
+		width              : "2.5em",
+		height             : "2.5em",
+		margin             : "0.5rem",
+		borderRadius       : "5px",
+		backgroundColor    : "hsla(15 100% 5% / 100%)",
+		border             : "0.1em solid hsla(15 100% 100% / 70%)",
+		// checked
+		"&:checked::before": { transform: "scale(1)" },
+		"&::before"        : {
+			transform      : "scale(0)",
+			display        : "inline-block",
+			content        : "''",
+			width          : "1.4em",
+			height         : "1.4em",
+			backgroundColor: "hsla(15 40% 50% / 100%)",
+			borderRadius   : "5px",
+		},
 	} );
 export class Task
 {
 	addToDOM( container )
 	{
 		const task = document.createElement( "div" ),
-			icon = document.createElement( "div" ),
+			check = document.createElement( "input" ),
 			title = document.createElement( "h3" ),
 			backgroundStyle = {
 				"medium": () => ( task.classList.add( css ( { backgroundColor: "hsla(15 100% 15% / 55%)" } ) ) ),
@@ -35,14 +58,12 @@ export class Task
 			};
 
 		( backgroundStyle[ this.priority ] || backgroundStyle.default )();
-		icon.dataset.icon = "flat-color-icons:medium-priority";
-		icon.classList.add( "iconify" );
-		icon.dataset.width = "2em";
+		check.type = "checkbox";
+		check.classList.add( checkStyle );
 		title.classList.add( titleStyle );
 		title.textContent = this.title;
-		// title.prepend( icon );
 		task.classList.add( taskStyle );
-		task.append( icon, title );
+		task.append( check, title );
 		container.prepend( task );
 	}
 	constructor( { title, description, priority, container } )
